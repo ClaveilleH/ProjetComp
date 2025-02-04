5909a207e9b94c22679e2d046ed9f87c642bc067
ClaveilleH <hugo@bobi.home>
2025-02-04 15:47:19 +0100

    Initial

---
+++ exec.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+SEARCH_DIR="fichiersProf/Tests"
+DONE=0
+ALL_OPTION=false
+HIDE_OPTION=false
+DOUBLE=false
+AA_OPTION=false
+
+for arg in "$@"; do
+    if [ -f "$arg" ]; then
+        echo $arg
+    else
+        if [ "$arg" == "--all" ]; then
+            ALL_OPTION=true
+            DOUBLE=true
+            continue
+        fi
+        if [ "$arg" == "--hide" ]; then
+            HIDE_OPTION=true
+            DOUBLE=true
+            continue
+        fi
+        if [ "$arg" == "--aa" ]; then
+            AA_OPTION=true
+            HIDE_OPTION=true
+            DOUBLE=true
+            continue
+        fi
+    fi
+done
+
+if [ $DOUBLE = false ]; then
+    while getopts "ah" opt; do
+        case $opt in
+            a)
+                ALL_OPTION=true
+                ;;
+            h)
+                HIDE_OPTION=true
+                ;;
+            *)  
+                echo "Usage: $0 [-a] [-h]"
+                exit 1
+                ;;
+        esac
+    done
+fi
+lex main.l && gcc lex.yy.c -o comp.out -ll && DONE=1
+
+
+
+
+# SI on a reussi a compiler
+if [ $DONE -eq 1 ]; then
+    if [ $ALL_OPTION = true ]; then
+        for file in "$SEARCH_DIR"/*; do
+            if [ -f "$file" ]; then
+                if [[ -f "$file" && "$file" == *.c ]]; then # si fichier .c
+                    if [ $HIDE_OPTION = true ]; then
+                        echo "Processing $file"
+                    fi
+                    if [ $AA_OPTION = true ]; then
+                        # echo "Processing $file"
+                        printf "Processing %s" $file
+                    fi
+                    cat "$file" | ./comp.out
+                fi
+            fi
+        done
+    else
+        if [ "$HIDE_OPTION" = false ]; then
+            echo "Processing fichiersProf/exempleminiC.c"
+        fi
+        cat fichiersProf/exempleminiC.c | ./comp.out
+    fi
+fi
